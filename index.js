fdd6a009af8f6808f03d111b8cf9fe4f128e414e
Takahiro Mitsuoka <tmitsuoka0423@gmail.com>
2023-03-04 03:50:11 +0900

    feat: 日本語で話しかける

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const openai = new OpenAIApi(configuration);
 const main = async () => {
   const completion = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: "Hello world" }],
+    messages: [{ role: "user", content: "こんにちは！" }],
   });
   console.log(completion.data.choices[0].message);
 }
